b61c20ded9e0568ab717c908da6a40877d629fcf
Dokeun Oh <ohdoking@gmail.com>
2025-08-16 05:50:00 +0200

    chore: add swagger docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
     // Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
