59a8860330975453d413c93eff8e38957ffec026
Dokeun Oh <ohdoking@gmail.com>
2025-08-16 04:45:24 +0200

    feat: implement Base62 Encoder

--- src/main/kotlin/com/dkb/urlshortener/core/service/UrlServiceImpl.kt
+++ src/main/kotlin/com/dkb/urlshortener/core/service/UrlServiceImpl.kt
@@ -5,13 +5,12 @@ import com.dkb.urlshortener.core.model.ShortenUrlDao
 import com.dkb.urlshortener.core.repository.ShortenUrlRepository
 import com.dkb.urlshortener.util.BaseEncoder
 import org.springframework.stereotype.Service
-import kotlin.io.encoding.Base64
 
 
 @Service
 class UrlServiceImpl(
     private val repository: ShortenUrlRepository,
-    private val base62Encoder: BaseEncoder
+    private val baseEncoder: BaseEncoder
 ) : UrlService {
 
     override fun createShortenUrl(originalUrl: String): ShortenUrlDao {
@@ -19,7 +18,7 @@ class UrlServiceImpl(
 
         var alias: String
         do {
-            alias = base62Encoder.encode(originalUrl)
+            alias = baseEncoder.encode(originalUrl)
         } while (repository.findByAlias(alias) != null)
 
         val dao = ShortenUrlDao(alias = alias, originalUrl = originalUrl)
--- src/main/kotlin/com/dkb/urlshortener/util/Base62Encoder.kt
+++ src/main/kotlin/com/dkb/urlshortener/util/Base62Encoder.kt
@@ -1,13 +1,36 @@
 package com.dkb.urlshortener.util
 
-class Base62Encoder: BaseEncoder {
+import org.springframework.stereotype.Component
+
+import java.math.BigInteger
+
+@Component
+class Base62Encoder : BaseEncoder {
+
+    private val ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    private val BASE = BigInteger.valueOf(ALPHABET.length.toLong())
 
     override fun encode(characters: String): String {
-        TODO("Not yet implemented")
+        val number = BigInteger(characters.toByteArray())
+        if (number == BigInteger.ZERO) return ALPHABET[0].toString()
+
+        val sb = StringBuilder()
+        var n = number
+        while (n > BigInteger.ZERO) {
+            val remainder = n.mod(BASE).toInt()
+            sb.append(ALPHABET[remainder])
+            n = n.divide(BASE)
+        }
+        return sb.reverse().toString()
     }
 
     override fun decode(characters: String): String {
-        TODO("Not yet implemented")
+        var num = BigInteger.ZERO
+        for (ch in characters) {
+            val index = ALPHABET.indexOf(ch)
+            if (index == -1) throw IllegalArgumentException("Invalid Base62 character: $ch")
+            num = num.multiply(BASE).add(BigInteger.valueOf(index.toLong()))
+        }
+        return String(num.toByteArray())
     }
-
 }
